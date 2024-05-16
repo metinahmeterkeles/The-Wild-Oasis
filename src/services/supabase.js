44a0a65035dc3c441e32629be6ee6e2a1e4e776d
Metin Ahmet Erkeleş <erkelesmetin@gmail.com>
2024-05-16 15:09:04 +0300

    uploading images to supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://yrttfbphkfyzlrtshqdu.supabase.co';
+export const supabaseUrl = 'https://yrttfbphkfyzlrtshqdu.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlydHRmYnBoa2Z5emxydHNocWR1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU0MzA1MjIsImV4cCI6MjAzMTAwNjUyMn0.fI4Ro9hLOdMLPYiUM6VtOxn3JHvzUj6aBdFEuYflXsc';
 const supabase = createClient(supabaseUrl, supabaseKey);
