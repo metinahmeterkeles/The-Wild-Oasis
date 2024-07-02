29c629c44a3d7eb91e1d0cdafb0d8ed7ba6bfb3b
Metin Ahmet Erkeles <erkelesmetin@gmail.com>
2024-07-02 23:46:20 +0300

    implemented dark mode

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -3,6 +3,7 @@ import ButtonIcon from '../ui/ButtonIcon';
 import { HiOutlineUser } from 'react-icons/hi';
 import { useNavigate } from 'react-router-dom';
 import Logout from '../features/authentication/Logout';
+import DarkModeToggle from './DarkModeToggle';
 
 const StyledHeadMenu = styled.ul`
   display: flex;
@@ -18,6 +19,9 @@ function HeaderMenu() {
         <ButtonIcon onClick={() => navigate('/account')}>
           <HiOutlineUser />
         </ButtonIcon>
+      </li>
+      <li>
+        <DarkModeToggle />
       </li>
       <li>
         <Logout />
