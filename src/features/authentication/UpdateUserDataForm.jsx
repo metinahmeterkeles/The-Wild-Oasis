5189572f45e531925dcc667fa2418e70b4d1dbef
Metin Ahmet Erkeles <erkelesmetin@gmail.com>
2024-07-02 14:57:16 +0300

    updating user data and password feature was added

--- src/features/authentication/UpdateUserDataForm.jsx
+++ src/features/authentication/UpdateUserDataForm.jsx
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import Button from "../../ui/Button";
-import FileInput from "../../ui/FileInput";
-import Form from "../../ui/Form";
-import FormRow from "../../ui/FormRow";
-import Input from "../../ui/Input";
+import Button from '../../ui/Button';
+import FileInput from '../../ui/FileInput';
+import Form from '../../ui/Form';
+import FormRow from '../../ui/FormRow';
+import Input from '../../ui/Input';
 
-import { useUser } from "./useUser";
+import { useUser } from './useUser';
+import { useUpdateUser } from './useUpdateUser';
 
 function UpdateUserDataForm() {
   // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
@@ -17,11 +18,29 @@ function UpdateUserDataForm() {
     },
   } = useUser();
 
-  const [fullName, setFullName] = useState(currentFullName);
+  const { updateUser, isUpdating } = useUpdateUser();
+
+  const [fullName, setFullName] = useState(
+    currentFullName.fullName || currentFullName
+  );
   const [avatar, setAvatar] = useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    updateUser(
+      { fullName, avatar },
+      {
+        onSuccess: () => {
+          setAvatar(null);
+          e.target.reset();
+        },
+      }
+    );
+  }
+
+  function handleCancel() {
+    setAvatar(null);
+    setFullName(currentFullName);
   }
 
   return (
@@ -35,6 +54,7 @@ function UpdateUserDataForm() {
           value={fullName}
           onChange={(e) => setFullName(e.target.value)}
           id="fullName"
+          disabled={isUpdating}
         />
       </FormRow>
       <FormRow label="Avatar image">
@@ -42,13 +62,19 @@ function UpdateUserDataForm() {
           id="avatar"
           accept="image/*"
           onChange={(e) => setAvatar(e.target.files[0])}
+          disabled={isUpdating}
         />
       </FormRow>
       <FormRow>
-        <Button type="reset" variation="secondary">
+        <Button
+          type="reset"
+          variation="secondary"
+          disabled={isUpdating}
+          onClick={handleCancel}
+        >
           Cancel
         </Button>
-        <Button>Update account</Button>
+        <Button disabled={isUpdating}>Update account</Button>
       </FormRow>
     </Form>
   );
---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import ButtonIcon from '../ui/ButtonIcon';
+import { HiOutlineUser } from 'react-icons/hi';
+import { useNavigate } from 'react-router-dom';
+import Logout from '../features/authentication/Logout';
+
+const StyledHeadMenu = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeadMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate('/account')}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeadMenu>
+  );
+}
+
+export default HeaderMenu;
