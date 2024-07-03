2b46c728d52de55b7e4026c8355850aa12eb2bf7
Metin Ahmet Erkeles <erkelesmetin@gmail.com>
2024-07-03 16:45:02 +0300

    project finished

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -4,7 +4,10 @@ import { createContext, useContext, useEffect } from 'react';
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, 'isDarkMode');
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    window.matchMedia('(prefers-color-scheme:dark)').matches,
+    'isDarkMode'
+  );
 
   useEffect(() => {
     if (isDarkMode) {
